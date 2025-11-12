2c71e133f43ee97c90dd4fbf05b80d0f5074d509
Selene Marisol Aban <michael.loves.cats.177@gmail.com>
2025-11-12 20:52:47 -0300

    feature/nivelDos: se modifico el script BusEnemyHealth definiendo el estado de vida de este Enemigo, aunque no lo reconoce el script EnemyAtaack ya que reconoce EnemyHealth así que, modificare ese script para que reconozca ambos enemigos aunque en este no se presenten los del nivel 1. -Aban Selene

--- Assets/Scripts/BusEnemyHealth.cs
+++ Assets/Scripts/BusEnemyHealth.cs
@@ -2,9 +2,43 @@ using UnityEngine;
 
 public class BusEnemyHealth : MonoBehaviour
 {
-    [Header("Salud del Colectivo")]
     public int maxHealth = 10;
     public int currentHealth;
 
     private BusEnemy busEnemy;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        busEnemy = GetComponent<BusEnemy>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerBullet"))
+        {
+            TakeDamage(1);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    public float GetHealthPercentage()
+    {
+        return (float)currentHealth / maxHealth;
+    }
 }
