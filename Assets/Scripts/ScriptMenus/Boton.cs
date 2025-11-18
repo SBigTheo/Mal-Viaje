5da6241c1d9d6349100abf2709a9a16f644bf3b1
Valentina <valexy7u7@gmail.com>
2025-11-18 16:15:47 -0300

    configurando los botones para cuando pase el raton cambie la imagen. Hecho con event Trigger

---
+++ Assets/Scripts/ScriptMenus/Boton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+// Script para efectos hover
+public class ButtonHoverEffect : MonoBehaviour
+{
+    [Header("Imagenes")]
+
+    public Sprite mariImagen;
+    public Sprite puchoImagen;
+
+    private Image botonImagen;
+    private Sprite originalImagen;
+
+    void Start() {
+        
+    }
+
+    public void CursorPasa()
+    {
+        
+    }
+
+    public void CursorYaNoPasa()
+    {
+        
+    }
+}
