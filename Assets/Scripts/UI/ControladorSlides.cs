5035ce4d3d6196eeb4561e223d05274af5540acc
Fátima <fatimacatacata51@gmail.com>
2025-11-25 21:31:46 -0300

    Cree un nuevo escenario en donde cargaremos todo lo que son las itros de nuestro juego

---
+++ Assets/Scripts/UI/ControladorSlides.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+
+public class ControladorSlides : MonoBehaviour
+{
+    [SerializeField] private Image imagenSlide;
+    [SerializeField] private List<Sprite> slides;
+    [SerializeField] private string escenaDestino = "Nivel1";
+
+    private int slideActual ;
+    private bool puedenAvanzar = false;
+    
+    void Start()
+    {
+        
+    }
+
+    void MOstrarSlideActual()
+    {
+        
+    }
+
+    public void AvanzarSlide()
+    {
+        
+    }
+
+    public void RetrocederSlide()
+    {
+        
+    }
+
+    private System.Collections.IEnumerator CooldownAvance()
+    {
+        
+    }
+
+    public void SaltarTodosSlides()
+    {
+        
+    }
+
+    private void CargarEscenarioDestino()
+    {
+        
+    }
+}
